9fe4d2ef5b940796996133101a1002fe494e57de
Floris Jetses <florisjoepie@gmail.com>
2025-12-05 16:34:17 +0100

    day-4

--- day-4/a.ts
+++ day-4/a.ts
@@ -1,9 +1,32 @@
-import { runSolution } from '../utils.ts';
+import { runSolution, toMatrix, traverseMatrix } from '../utils.ts';
+import { directions } from '../utils/directions.ts';
 
 /** provide your solution as the return of this function */
 export async function day4a(data: string[]) {
-  console.log(data);
-  return 0;
+  const matrix = toMatrix(data);
+  const maxRolls = 3;
+  let accessibleRolls = 0;
+
+  traverseMatrix(matrix, (val, x, y) => {
+    if (val !== '@') return;
+    let nearbyRolls = 0;
+    Object.values(directions).forEach((dir) => {
+      const [xdir, ydir] = dir;
+      const [xpos, ypos] = [x + xdir, y + ydir];
+      if (ypos < 0 || ypos >= matrix.length) return;
+      if (xpos < 0 || xpos >= matrix[0].length) return;
+      const pos = matrix[ypos][xpos];
+
+      if (pos === '@') {
+        nearbyRolls += 1;
+      }
+    });
+
+    if (nearbyRolls <= maxRolls) {
+      accessibleRolls += 1;
+    }
+  });
+  return accessibleRolls;
 }
 
 await runSolution(day4a);
--- day-4/b.ts
+++ day-4/b.ts
@@ -1,9 +1,51 @@
-import { runSolution } from '../utils.ts';
+import { runSolution, toMatrix, traverseMatrix } from '../utils.ts';
+import { directions } from '../utils/directions.ts';
 
 /** provide your solution as the return of this function */
 export async function day4b(data: string[]) {
-  console.log(data);
-  return 0;
+  const matrix = toMatrix(data);
+  const maxRolls = 3;
+  let removableRolls: [number, number][] = [];
+  let rollsRemoved = 0;
+
+  const removeRolls = () => {
+    traverseMatrix(matrix, (val, x, y) => {
+      if (val !== '@') return;
+      let nearbyRolls = 0;
+      Object.values(directions).forEach((dir) => {
+        const [xdir, ydir] = dir;
+        const [xpos, ypos] = [x + xdir, y + ydir];
+        if (ypos < 0 || ypos >= matrix.length) return;
+        if (xpos < 0 || xpos >= matrix[0].length) return;
+        const pos = matrix[ypos][xpos];
+
+        if (pos === '@') {
+          nearbyRolls += 1;
+        }
+      });
+
+      if (nearbyRolls <= maxRolls) {
+        removableRolls.push([x, y]);
+      }
+    });
+
+    const hasRolls = !!removableRolls.length;
+
+    removableRolls.forEach((coords) => {
+      const [x, y] = coords;
+      matrix[y][x] = 'x';
+      rollsRemoved += 1;
+    });
+    removableRolls = [];
+
+    if (hasRolls) {
+      removeRolls();
+    }
+  };
+
+  removeRolls();
+
+  return rollsRemoved;
 }
 
 await runSolution(day4b);
