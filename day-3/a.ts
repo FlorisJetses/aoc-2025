215720549e388c845e25d8d0ab69e56e3d39c8ea
Floris Jetses <florisjoepie@gmail.com>
2025-12-05 16:12:43 +0100

    day-3

--- day-3/a.ts
+++ day-3/a.ts
@@ -2,8 +2,26 @@ import { runSolution } from '../utils.ts';
 
 /** provide your solution as the return of this function */
 export async function day3a(data: string[]) {
-  console.log(data);
-  return 0;
+  let sum = 0;
+  data.forEach((line) => {
+    if (!line) {
+      return;
+    }
+    let firstLargestNumber = 0;
+    let secondLargestNumber = 0;
+    line.split('').forEach((char, index, arr) => {
+      const number = parseInt(char, 10);
+      if (number > firstLargestNumber && index !== arr.length - 1) {
+        firstLargestNumber = number;
+        secondLargestNumber = 0;
+      } else if (number > secondLargestNumber) {
+        secondLargestNumber = number;
+      }
+    });
+    const total = Number(`${firstLargestNumber}${secondLargestNumber}`);
+    sum += total;
+  });
+  return sum;
 }
 
 await runSolution(day3a);
--- day-3/b.ts
+++ day-3/b.ts
@@ -2,8 +2,35 @@ import { runSolution } from '../utils.ts';
 
 /** provide your solution as the return of this function */
 export async function day3b(data: string[]) {
-  console.log(data);
-  return 0;
+  let sum = 0;
+  data.forEach((line) => {
+    if (!line) {
+      return;
+    }
+    const highestNumbers: number[] = [...Array(12).fill(-1)];
+    let currentCharIndex = 0;
+
+    for (let i = 0; i < highestNumbers.length; i++) {
+      let largestNumber = -1;
+      for (
+        let j = currentCharIndex;
+        j < line.length - (highestNumbers.length - 1 - i);
+        j++
+      ) {
+        const number = parseInt(line[j], 10);
+        if (number > largestNumber) {
+          largestNumber = number;
+          currentCharIndex = j + 1;
+        }
+      }
+      highestNumbers[i] = largestNumber;
+    }
+
+    const total = Number(`${highestNumbers.join('')}`);
+    sum += total;
+    console.log(total);
+  });
+  return sum;
 }
 
 await runSolution(day3b);
