8fd5b1f174b9e93e36ca1cb3e4af96c7fa74e0f2
Floris Jetses <florisjoepie@gmail.com>
2025-12-08 08:20:44 +0100

    day 5

--- day-5/a.ts
+++ day-5/a.ts
@@ -1,9 +1,46 @@
 import { runSolution } from '../utils.ts';
 
+const isInRange = (num: number, start: number, end: number) => {
+  return num >= start && num <= end;
+};
+
 /** provide your solution as the return of this function */
 export async function day5a(data: string[]) {
-  console.log(data);
-  return 0;
+  const ids: string[] = [];
+  const ingredients: string[] = [];
+  let freshAmount = 0;
+
+  let hasIds = false;
+  data.forEach((item) => {
+    if (item === '') {
+      hasIds = true;
+      return;
+    }
+
+    if (hasIds && item) {
+      ingredients.push(item);
+      return;
+    }
+
+    if (item) {
+      ids.push(item);
+    }
+  });
+
+  ingredients.forEach((id) => {
+    let isFresh = false;
+    ids.forEach((range) => {
+      if (isFresh) return;
+      const [start, end] = range.split('-');
+
+      if (isInRange(Number(id), Number(start), Number(end))) {
+        isFresh = true;
+      }
+    });
+    if (isFresh) freshAmount++;
+  });
+
+  return freshAmount;
 }
 
 await runSolution(day5a);
--- day-5/b.ts
+++ day-5/b.ts
@@ -1,9 +1,82 @@
 import { runSolution } from '../utils.ts';
 
+const isInRange = (num: number, start: number, end: number) => {
+  return num >= start && num <= end;
+};
+
 /** provide your solution as the return of this function */
 export async function day5b(data: string[]) {
-  console.log(data);
-  return 0;
+  const ids: string[] = [];
+  const ingredients: string[] = [];
+
+  let hasIds = false;
+  data.forEach((item) => {
+    if (item === '') {
+      hasIds = true;
+      return;
+    }
+
+    if (hasIds && item) {
+      ingredients.push(item);
+      return;
+    }
+
+    if (item) {
+      ids.push(item);
+    }
+  });
+
+  const starts: number[] = [];
+  const startEnd: Record<number, number> = {};
+  const ends: number[] = [];
+  const endStart: Record<number, number> = {};
+
+  const ranges: [number, number][] = [];
+
+  ids.forEach((range) => {
+    const [start, end] = range.split('-');
+
+    starts.push(Number(start));
+    startEnd[Number(start)] = Number(end);
+    ends.push(Number(end));
+    endStart[Number(end)] = Number(start);
+    ranges.push([Number(start), Number(end)]);
+  });
+
+  const sortedRanges = ranges.sort((a, b) => {
+    return a[0] - b[0];
+  });
+
+  const newRanges: [number, number][] = [];
+
+  sortedRanges.forEach((range, index) => {
+    if (index === 0) {
+      newRanges.push(range);
+    }
+
+    const previousRange = newRanges.at(-1);
+    if (previousRange[0] <= range[0] && previousRange[1] >= range[1]) {
+      return;
+    }
+
+    if (
+      previousRange[0] <= range[0] &&
+      isInRange(previousRange[1], range[0], range[1])
+    ) {
+      newRanges[newRanges.length - 1] = [previousRange[0], range[1]];
+      return;
+    }
+
+    newRanges.push(range);
+  });
+
+  let sum = 0;
+
+  newRanges.forEach((range) => {
+    sum += range[1] - range[0] + 1;
+  });
+
+  return sum;
 }
 
 await runSolution(day5b);
